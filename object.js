b920b107b67d8ae944b0643cf488a2aca3c7cc2c
Vadim <www.vadim.2@mail.ru>
2022-03-28 14:50:56 +0300

    v 1

--- object.js
+++ object.js
@@ -2,5 +2,30 @@ const Vadim = {
     name: 'Вадим',
     lastName: 'Метла',
     dateOfBrith: '22/04/1996',
-    reason: 'Работа головой'
-}
+    reason: 'Хочу работать головой',
+    logName () {
+        console.log('Моё имя: ', this.name);
+    },
+    logLastName () {
+        console.log('Моя фамилия: ', this.lastName);
+    },
+    logBrith () {
+        console.log('Моя дата рождения: ', this.dateOfBrith);
+    },
+    logReason () {
+        console.log(this.name,'хочет выучить JS потому что ', this.reason);
+    },
+    dream (audi) {
+        console.log(this.name, 'мечтает об ', audi);
+
+    },
+    Information () {
+        this.logName();
+        this.logLastName();
+        this.logBrith();
+        this.logReason();
+        this.dream('Ауди с автосалона');
+    }
+};
+//Vadim.dream('Ауди с автосалона');
+Vadim.Information();
